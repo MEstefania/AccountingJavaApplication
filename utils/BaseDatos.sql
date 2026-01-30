f59f7c395158582ff46a9fc80720c26253ac64ca
Mishell C. <jose.catota@snappcorp.com>
2026-01-29 22:48:43 -0500

    feat(bbdd): cambio de nombre de bbdd

--- utils/BaseDatos.sql
+++ utils/BaseDatos.sql
@@ -1,4 +1,4 @@
-create database accounting_db;
+create database customer_db;
 CREATE USER tata_user WITH PASSWORD 'password';
 CREATE USER tata_owner WITH PASSWORD 'password';
 ALTER USER tata_owner WITH SUPERUSER;
